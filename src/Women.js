616cac32f1021583dae07a52b0c9a9a82bd0b300
christopherleveston <christopherleveston@outlook.com>
2019-05-10 08:57:41 -0500

    added more contents to the womens page

--- src/Women.js
+++ src/Women.js
@@ -22,25 +22,25 @@ class Women extends Component {
         }
         return(
             <div>
-                <h2 style={ tableTitleStyle }>Womens</h2>
+                <h2 style={ tableTitleStyle }>WOMENS</h2>
                 <table style= { tableStyle }>
                     <tr>
                         <td style={ tableDataStyle }>
-                            <img style={ tableImages } src={require("./images/BlissfullyLivingShirtMEN.PNG")}/>
+                            <img style={ tableImages } src={require("./images/WBlissfullyLiving.PNG")}/>
                             <p>Blissfully Living</p>
                             <h4>$30.00</h4>
                             <a href="#"><button>View</button></a>
                         </td>
 
                         <td style={ tableDataStyle }>
-                            <img style={ tableImages } src={require("./images/LetsHaveABeerMENS.PNG")}/>
+                            <img style={ tableImages } src={require("./images/WLetsHaveABeer.PNG")}/>
                             <p>Let's Have A Beer</p>
                             <h4>$30.00</h4>
                             <a href="#"><button>View</button></a>
                         </td>
 
                         <td style={ tableDataStyle }>
-                            <img style={ tableImages } src={require("./images/AllMight,AllPowerMENS.PNG")}/>
+                            <img style={ tableImages } src={require("./images/WAllMight,AllPower.PNG")}/>
                             <p>All Might, All Power</p>
                             <h4>$30.00</h4>
                             <a href="#"><button>View</button></a>
@@ -49,22 +49,30 @@ class Women extends Component {
 
                     <tr>
                         <td style={ tableDataStyle } >
-                            <img style={ tableImages } src={require("./images/IKeepItOneHundredMENS.PNG")}/>
+                            <img style={ tableImages } src={require("./images/WIKeepItOneHundred.PNG")}/>
                             <p>I Keep It One Hundred!!!</p>
                             <h4>$30.00</h4>
                             <a href="#"><button>View</button></a>
                         </td>
 
                         <td style={ tableDataStyle }>
-                            <img style={ tableImages } src={require("./images/AmaterasuMENS.PNG")}/>
+                            <img style={ tableImages } src={require("./images/WAmaterasu.PNG")}/>
                             <p>Amaterasu</p>
                             <h4>$30.00</h4>
                             <a href="#"><button>View</button></a>
                         </td>
 
                         <td style={ tableDataStyle }>
-                            <img style={ tableImages } src={require("./images/TheWorldIsMyCanvasMENS.PNG")}/>
+                            <img style={ tableImages } src={require("./images/WTheWorldIsMyCanvas.PNG")}/>
                             <p>The World is My Canvas</p>
+                            <h4>$30.00</h4>
+                            <a href="#"><button>View</button></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style={ tableDataStyle }>
+                            <img style={ tableImages } src={require("./images/WNoodleGeisha.PNG")}/>
+                            <p>Noodle Geisha</p>
                             <h4>$30.00</h4>
                             <a href="#"><button>View</button></a>
                         </td>
