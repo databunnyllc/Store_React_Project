8ce57fd22a87a29c98e49ce30950f9f3695ee1bf
christopherleveston <christopherleveston@outlook.com>
2019-05-10 06:30:45 -0500

    Updates

---
+++ precache-manifest.ec21472d7c8a8ecc739a3b9e2d958068.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "9af9db2ba6eb70fcba2c0579230cc09e",
+    "url": "/static/media/slide2.9af9db2b.png"
+  },
+  {
+    "revision": "56364c0f60c1b21d0392",
+    "url": "/static/css/main.649bd318.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d8d1431587e5f4698c52",
+    "url": "/static/js/2.d8d14315.chunk.js"
+  },
+  {
+    "revision": "e4e4123eaa457f7c116bf63116c6f660",
+    "url": "/static/media/shoppingBag.e4e4123e.png"
+  },
+  {
+    "revision": "dce9bf46e3acb63de3d796ffd9415fde",
+    "url": "/static/media/Leveston-DesignsLOGO.dce9bf46.png"
+  },
+  {
+    "revision": "c10f9013d028caa0bf464415e05a5435",
+    "url": "/static/media/boxImage1.c10f9013.png"
+  },
+  {
+    "revision": "8e33bef420dac6187738aedfdc0d46fa",
+    "url": "/static/media/boxImage2.8e33bef4.png"
+  },
+  {
+    "revision": "69a4b4ebe5adf96cfd5c51361bc59d6c",
+    "url": "/static/media/boxImage3.69a4b4eb.png"
+  },
+  {
+    "revision": "3485124cd9d7359bbd1ec9610f122058",
+    "url": "/static/media/slide1.3485124c.png"
+  },
+  {
+    "revision": "56364c0f60c1b21d0392",
+    "url": "/static/js/main.56364c0f.chunk.js"
+  },
+  {
+    "revision": "48daa71f73196c3a08068548649f2e43",
+    "url": "/static/media/slide3.48daa71f.png"
+  },
+  {
+    "revision": "a65577868b000ba5cacb31049688d3d7",
+    "url": "/static/media/BlissfullyLivingShirtMEN.a6557786.PNG"
+  },
+  {
+    "revision": "5d557b0d65a2bb5fd04a21a112bab6e6",
+    "url": "/static/media/LetsHaveABeerMENS.5d557b0d.PNG"
+  },
+  {
+    "revision": "68f266843e225e2aa55968915f8942bc",
+    "url": "/static/media/AllMight,AllPowerMENS.68f26684.PNG"
+  },
+  {
+    "revision": "e6bce3e8c08a5d505b1d0ed2cceb0f9d",
+    "url": "/static/media/IKeepItOneHundredMENS.e6bce3e8.PNG"
+  },
+  {
+    "revision": "415faa5bb531304cf3dfd0f43a1e70f1",
+    "url": "/static/media/AmaterasuMENS.415faa5b.PNG"
+  },
+  {
+    "revision": "aa93a5abb2698bc9c88a6e76bde6bd57",
+    "url": "/static/media/TheWorldIsMyCanvasMENS.aa93a5ab.PNG"
+  },
+  {
+    "revision": "38e53d7d873457158da0b2d79515305c",
+    "url": "/static/media/facebookIcon.38e53d7d.png"
+  },
+  {
+    "revision": "51e6c64b04577f84af6c87255aefef2c",
+    "url": "/static/media/instagramIcon.51e6c64b.png"
+  },
+  {
+    "revision": "d653681e63040f11c4e532ffa351abd6",
+    "url": "/index.html"
+  }
+];
